09faea54fc522711c516b9b194258762559decc1
Briniel <lord.devol@gmail.com>
2021-08-20 12:39:26 +0300

    Создал приложение, которое меняет цвет view в зависимости от положения slide Определение идет по 3 цветам

--- Palette/ViewController.swift
+++ Palette/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var viewPalette: UIView!
+    
+    @IBOutlet var slideRed: UISlider!
+    
+    @IBOutlet var slideGreen: UISlider!
+    @IBOutlet var slideBlue: UISlider!
+    
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewPalette.layer.cornerRadius = 10
+        labelRed.text = String(slideRed.value)
+        labelGreen.text = String(slideGreen.value)
+        labelBlue.text = String(slideBlue.value)
     }
-
-
+    
+    @IBAction func changeColor() {
+        viewPalette.backgroundColor = UIColor(red: CGFloat(slideRed.value), green: CGFloat(slideGreen.value), blue: CGFloat(slideBlue.value), alpha: 1)
+        
+        labelRed.text = String(round(100 * slideRed.value)/100)
+        labelGreen.text = String(round(100 * slideGreen.value)/100)
+        labelBlue.text = String(round(100 * slideBlue.value)/100)
+    }
 }
-
